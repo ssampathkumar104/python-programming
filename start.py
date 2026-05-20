d6e81cb3a7e21af8cdcdacc0f862058249a3e450
Sampath Sankati <Sampath.Sankati@infor.com>
2026-05-20 21:59:18 +0530

    first file

---
+++ start.py
@@ -0,0 +1,28 @@
+
+print("This is the beginning of a new journey.")
+print(2+3) # Addition   
+print(2+3.5) # Addition with a float, the result will be a float
+print(2*5) # Multiplication 
+print(10/2) # Division, the result will be a float
+print(10//2) # Floor division, the result will be an integer
+print(10%3) # Modulus, the result will be the remainder of the division 
+print("2+3.5") # Anything kept inside quotes is treated as a string, so this will just print "2+3.5" without performing the addition.
+print("The result of 2+3 is:", 2+3) # This will print the string followed by the result of the addition.
+
+## Variables and data types
+string_var = "Hello, World!" # This is a string variable
+int_var = 42 # This is an integer variable  
+float_var = 3.14 # This is a float variable
+bool_var = True # This is a boolean variable
+print(string_var) # This will print the string variable
+print(int_var) # This will print the integer variable   
+print(float_var) # This will print the float variable
+print(bool_var) # This will print the boolean variable
+
+username="Sampath"
+print(len(username)) # This will print the length of the string variable "username"
+print(username[0]) # This will print the first character of the string variable "username"
+print(username[1:4]) # This will print the characters from index 1 to
+
+password=input("Enter your password: ") # This will prompt the user to enter a password and store it in the variable "password"
+print("Your password is:", password) # This will print the password entered by the user
